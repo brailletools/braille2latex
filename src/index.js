6b732e9015b4925c42ca6cbff73ec9dc3d9d534b
Jennifer Mankoff <jmankoff@cs.washington.edu>
2026-07-08 09:09:16 -0700

    Export braille2Ascii from package index

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 export { configure, parse } from './processFile.js';
-export { nemeth_to_latex, ascii2Braille } from './brailleMap.js';
+export { nemeth_to_latex, ascii2Braille, braille2Ascii } from './brailleMap.js';
